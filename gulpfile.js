005e86288deef124ce0602e28541107afdb45751
Simon Bloom <swbloom@gmail.com>
2017-01-16 14:28:07 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp');
+const babel = require('babelify');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+
+gulp.task('js', () => {
+	browserify('src/app.js')
+		.transform('babelify', {
+			presets: ['es2015','react']
+		})
+		.bundle()
+		.on('error',notify.onError({
+			message: "Error: <%= error.message %>",
+			title: 'Error in JS 💀'
+		}))
+		.pipe(source('app.js'))
+		.pipe(buffer())
+		.pipe(gulp.dest('public/'))
+		.pipe(reload({stream:true}));
+});
+
+gulp.task('bs', () => {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
+});
+
+gulp.task('default', ['js','bs'], () => {
+	gulp.watch('src/**/*.js',['js']);
+	gulp.watch('./public/style.css',reload);
+});
